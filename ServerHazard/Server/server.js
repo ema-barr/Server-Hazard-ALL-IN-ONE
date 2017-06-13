63b173bdb2774c09bc5cc6f8ef7c92231cc9dbc6
CosimoLovascio <cosimolovascio@hotmail.it>
2017-06-13 10:48:29 +0200

    Update project

--- ServerHazard/Server/server.js
+++ ServerHazard/Server/server.js
@@ -6,14 +6,13 @@ var http = require('http'),
 dashboard = fs.readFileSync(__dirname + '/mockdash.html');
 fakeboard = fs.readFileSync(__dirname + '/fakeboard.html')
 
-
-
 //Server port
 var port = 6882;
 
 //Web server
 var app = express();
 var server = http.createServer(app);
+server.listen(port, "0.0.0.0");
 
 //Game Engine socket
 var gameEngineSocketID;
@@ -116,68 +115,6 @@ io.on('connection', function (socket) {
         handleChooseProductionCard('chooseProductionCard', data, callback);
     });
 
-    function handleRequestAndNotifyDashboard(requestName, data, callback) {
-        console.log("Request received. Name: " + requestName + ", \nData:");
-        console.log(data);
-        //Route the request to the appropriate method
-        request(requestName, data, function(response) {
-            responseJ = JSON.parse(response);
-            logString = responseJ.logString;
-            success = responseJ.success;
-            stateRequest = {"requestName": "getState"};
-            //Request the state to send to the dashboard
-            sendUpdateToDashboard(response, function() {
-                callback(response);
-            });
-        });
-    }
-
-    function sendMessage(socketID, messageName, data) {
-        if (io.sockets.connected[socketID] != undefined) {
-            io.sockets.connected[socketID].emit(messageName, data);
-        }
-    }
-
-    function sendUpdateToDashboard(response, callback) {
-        //Request the state to send to the dashboard
-        request("getState", {}, function(getStateResponse) {
-            //Send the state to the dashboard
-            newResponse = {};
-            newResponse.state = JSON.parse(getStateResponse);
-            newResponse.response = JSON.parse(response);
-            console.log(newResponse);
-            //io.sockets.connected[dashboardSocketID].emit('update', newResponse);
-            sendMessage(dashboardSocketID, 'update', newResponse);
-
-            //var commandToSend = arduinoLedCommand(newResponse.state);
-            //TODO send command to board
-
-            callback();
-        })
-    }
-
-    function handleChooseProductionCard(requestName, data, callback) {
-        dataJ = data;
-        cardIndex = dataJ.cardIndex;
-        handleStandardRequest("chooseProductionCard", data, function(response) {
-            responseJ = JSON.parse(response);
-            request("getState", {}, function(getStateResponse) {
-                stateResponseJ = JSON.parse(getStateResponse);
-                currentTurnJ = stateResponseJ.currentTurn;
-                //If the production cards have been selected, notify the device so it can show the production group
-                //interface.
-                if (responseJ.success && currentTurnJ.state === "MOVE_TRANSPORT_PAWN") {
-                    sendMessage(clientSocketID, 'productionStateChanged', {});
-                }
-                newResponse = {};
-                newResponse.state = stateResponseJ;
-                newResponse.response = responseJ;
-                newResponse.cardIndex = cardIndex;
-                sendMessage(dashboardSocketID, 'chooseProductionCard', newResponse);
-            });
-            callback(response);
-        });
-    }
     /*
      function handleNextTurn(requestName, data, callback) {
      //Close all pending popup messages
@@ -224,29 +161,102 @@ io.on('connection', function (socket) {
      }
      }
      */
-    function handleStandardRequest(requestName, data, callback) {
-        console.log("Request received. Name: " + requestName + ", \nData:");
-        console.log(data);
-        request(requestName, data, callback);
-    }
 
-    function request(requestName, data, callback) {
-        console.log("requestName " + requestName + ", data " + data);
-        if(typeof data == 'string')
-            var reqData = JSON.parse(data);
-        else {
-            var reqData = {};
-            reqData = data;
-        }
-        //Add the request name to the JSON request data
-        reqData["requestName"] = requestName;
-        //Send the new request object to the game engine
-        console.log("reqData " + reqData );
-        io.sockets.connected[gameEngineSocketID].emit('request', reqData, function(response) {
+});
+
+function handleRequestAndNotifyDashboard(requestName, data, callback) {
+    console.log("Request received. Name: " + requestName + ", \nData:");
+    console.log(data);
+    //Route the request to the appropriate method
+    request(requestName, data, function(response) {
+        responseJ = JSON.parse(response);
+        logString = responseJ.logString;
+        success = responseJ.success;
+        stateRequest = {"requestName": "getState"};
+        //Request the state to send to the dashboard
+        sendUpdateToDashboard(response, function() {
             callback(response);
         });
+    });
+}
+
+function sendMessage(socketID, messageName, data) {
+    if (io.sockets.connected[socketID] != undefined) {
+        io.sockets.connected[socketID].emit(messageName, data);
     }
-});
+}
+
+function sendUpdateToDashboard(response, callback) {
+    //Request the state to send to the dashboard
+    request("getState", {}, function(getStateResponse) {
+        //Send the state to the dashboard
+        newResponse = {};
+        newResponse.state = JSON.parse(getStateResponse);
+        newResponse.response = JSON.parse(response);
+        console.log(newResponse);
+        //io.sockets.connected[dashboardSocketID].emit('update', newResponse);
+        sendMessage(dashboardSocketID, 'update', newResponse);
+
+        //create led command code
+        var ledCommandToSend = arduinoLedCommand(newResponse.state);
+        sendCommandToArduino(ledCommandToSend);
+
+        //4 seconds delay
+        sleep(4000);
+
+        //create sound command code
+        var soundCommandToSend = arduinoSoundCommand(newResponse.state);
+        sendCommandToArduino(soundCommandToSend);
+
+        callback();
+    })
+}
+
+function handleStandardRequest(requestName, data, callback) {
+    console.log("Request received. Name: " + requestName + ", \nData:");
+    console.log(data);
+    request(requestName, data, callback);
+}
+
+function request(requestName, data, callback) {
+    console.log("requestName " + requestName + ", data " + data);
+    if(typeof data == 'string')
+        var reqData = JSON.parse(data);
+    else {
+        var reqData = {};
+        reqData = data;
+    }
+    //Add the request name to the JSON request data
+    reqData["requestName"] = requestName;
+    //Send the new request object to the game engine
+    console.log("reqData " + reqData );
+    io.sockets.connected[gameEngineSocketID].emit('request', reqData, function(response) {
+        callback(response);
+    });
+}
+
+function handleChooseProductionCard(requestName, data, callback) {
+    dataJ = data;
+    cardIndex = dataJ.cardIndex;
+    handleStandardRequest("chooseProductionCard", data, function(response) {
+        responseJ = JSON.parse(response);
+        request("getState", {}, function(getStateResponse) {
+            stateResponseJ = JSON.parse(getStateResponse);
+            currentTurnJ = stateResponseJ.currentTurn;
+            //If the production cards have been selected, notify the device so it can show the production group
+            //interface.
+            if (responseJ.success && currentTurnJ.state === "MOVE_TRANSPORT_PAWN") {
+                sendMessage(clientSocketID, 'productionStateChanged', {});
+            }
+            newResponse = {};
+            newResponse.state = stateResponseJ;
+            newResponse.response = responseJ;
+            newResponse.cardIndex = cardIndex;
+            sendMessage(dashboardSocketID, 'chooseProductionCard', newResponse);
+        });
+        callback(response);
+    });
+}
 
 app.get('/', function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
@@ -264,24 +274,33 @@ app.get('/fakeboard', function (req, res) {
     res.end(fakeboard);
 });
 
+app.get('/get_production_card', function (req, res) {
+    console.log("Hai premuto il pulsante: " + req.query.cardIndex);
+    handleChooseProductionCard("chooseProductionCard", {cardIndex: req.query.cardIndex}, null);
+    res.end("");    //return empty string to Arduino
+});
+
 fs = require('fs');
+requestArduino = require('request');
+
 try {
-    var xmlConfiguration = fs.readFileSync('./configuration.xml', 'utf8');
-    var DOMParser = require('xmldom').DOMParser;
+	var xmlConfiguration = fs.readFileSync('../strutturaxml.xml', 'utf8');
+	var DOMParser = require('xmldom').DOMParser;
 } catch(err) {
-    console.log("Configuration file not found.");
-    var xmlConfiguration = null;
+	console.log("Configuration file not found.");
+	var xmlConfiguration = null;
 }
 
 setupGlobalVariables(xmlConfiguration);
 
 function setupGlobalVariables(xmlConfigurationFile) {
-    if (xmlConfiguration != null) {
-        console.log("Setting up global game variables...");
-        gravityLevelsArray = setupGravityLevels(xmlConfigurationFile);
-        ledEmergencyMap = setupLedEmergencyMap(xmlConfigurationFile);
-        setupBoardSettings(xmlConfigurationFile);
-    }
+	if (xmlConfiguration != null) {
+		console.log("Setting up global game variables...");
+	    gravityLevelsArray = setupGravityLevels(xmlConfigurationFile);
+	    ledEmergencyMap = setupLedEmergencyMap(xmlConfigurationFile);
+	    soundCodeMap = setupSoundCodeMap(xmlConfigurationFile);
+	    setupBoardSettings(xmlConfigurationFile);
+	}
 }
 
 function setupGravityLevels(xmlConfigurationFile) {
@@ -306,10 +325,10 @@ function setupLedEmergencyMap(xmlConfigurationFile) {
     var locations = [];
 
     for (var i = 0; i < areas.length; i++) {
-        locations.push(areas[i].getElementsByTagName("location")[0]);
+        locations.push.apply(locations, areas[i].getElementsByTagName("location"));
     }
 
-    var ledEmergencyMap = [[]];
+    var ledEmergencyMap = [];
     for (var i = 0; i < locations.length; i++) {
         var locationName = locations[i].getElementsByTagName("name")[0].textContent;
         var emergencyArray = [];
@@ -321,12 +340,26 @@ function setupLedEmergencyMap(xmlConfigurationFile) {
             number = leds[j].getElementsByTagName("number")[0].textContent;
             emergency = leds[j].getElementsByTagName("emergency")[0].textContent;
             emergencyArray[emergency] = number;
-            emergencyArray;
         }
         ledEmergencyMap[locationName] = emergencyArray;
     }
 
     return ledEmergencyMap;
+}
+
+function setupSoundCodeMap(xmlConfigurationFile) {
+    var parser = new DOMParser();
+    var xmlParser = parser.parseFromString(xmlConfigurationFile, "text/xml");
+    var soundSetting = xmlParser.getElementsByTagName("arduinoSoundSettings")[0];
+    var actions =  soundSetting.getElementsByTagName("action");
+    var soundCodeMap = [];
+
+    for (var i = 0; i < actions.length; i++) {
+        var actionName = actions[i].getElementsByTagName("name")[0].textContent;
+        var soundCode = actions[i].getElementsByTagName("soundCode")[0].textContent;
+        soundCodeMap[actionName] = soundCode;
+    }
+    return soundCodeMap;
 }
 
 function setupBoardSettings(xmlConfigurationFile) {
@@ -347,10 +380,10 @@ function arduinoLedCommand(gameState) {
     var locations = gameState['gameState']['gameMap']['locations'];
 
     /*
-     create a dictionary with 2 key:
-     first key is location name (contain a dictionary),
-     second key is emergency name and contain the emergency level
-     */
+    create a dictionary with 2 key:
+    first key is location name (contain a dictionary),
+    second key is emergency name and contain the emergency level
+    */
     var mapLocationLedColor = {};
 
     //extract location list
@@ -368,24 +401,80 @@ function arduinoLedCommand(gameState) {
     }
 
     //initialization string to send at arduino
-    var ledCommandArdino =  commandCode + ledCommandCode;
+    var ledCommandArdino =  "";
     for(var i = 0; i < ledNumber; i++) {
         ledCommandArdino += "0";
     }
-    ledCommandArdino += commandCode;
-
 
     //set led colors using map association and emergency
     for(var loc in mapLocationLedColor){
         for (var emergency in mapLocationLedColor[loc]){
+
             //change default value in the ledCommandArdino if exist the led in the map
-            if(ledMap[loc][emergency]){
-                ledCommandArdino[ledEmergencyMap[loc][emergency]] = mapLocationLedColor[loc][emergency];
+            if(ledEmergencyMap[loc][emergency]){
+                var index = ledEmergencyMap[loc][emergency];
+                var substringLeft = ledCommandArdino.substring(0, index);
+                var substringRight = ledCommandArdino.substring(++index, ledCommandArdino.length);
+                ledCommandArdino = substringLeft + gravityLevelsArray[mapLocationLedColor[loc][emergency]] + substringRight;
             }
         }
     }
 
+    //append instruction type to the led string command
+    ledCommandArdino = commandCode + ledCommandCode + ledCommandArdino;
+    ledCommandArdino += commandCode;
+
     return ledCommandArdino;
+}
+
+function arduinoSoundCommand(gameState) {
+
+    //get the action name
+    var action = gameState.response.response.actionName;
+
+    //if the action is an event
+    if (action == "EVENT_TURN_START") {
+        action = gameState.response.response.responses[0].actionName;
+    }
+
+    var soundCode = soundCodeMap[action];
+    if (!soundCode) {
+        soundCode = "0";    //default value
+    }
+
+    //create the command to send
+    var soundCommandArdino =  "";
+
+    //need a long string
+    for(var i = 0; i < 99; i++) {
+        soundCommandArdino += "0";
+    }
+
+    soundCommandArdino = commandCode + soundCommandCode + soundCommandArdino;
+    soundCommandArdino += soundCode;
+    soundCommandArdino += commandCode;
+
+    return soundCommandArdino;
+}
+
+function sendCommandToArduino(command) {
+    var url = "http://localhost:6883/command/" + command;
+    console.log(url);
+    requestArduino.get(
+        url,
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log("Command sent: " + command);
+            }
+        }
+    );
+}
+
+function sleep(time) {
+    var stop = new Date().getTime();
+    while(new Date().getTime() < stop + time) {
+        continue;
+    }
 }
 
 server.listen(port);
