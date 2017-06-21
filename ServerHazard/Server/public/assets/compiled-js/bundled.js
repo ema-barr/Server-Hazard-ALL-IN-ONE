9f2f2367115a65dd6086d9584d34abc79ed07eac
pmarco0 <pintomarco@gmx.com>
2017-06-21 13:59:14 +0200

    BugFix Blockades (Dashboard)

--- ServerHazard/Server/public/assets/compiled-js/bundled.js
+++ ServerHazard/Server/public/assets/compiled-js/bundled.js
@@ -14847,6 +14847,31 @@
 					return a < b ? this >= a && this <= b : this >= b && this <= a;
 				};
 
+				// Warn if overriding existing method
+				if (Array.prototype.equals) console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.");
+				// attach the .equals method to Array's prototype to call it on any array
+				Array.prototype.equals = function (array) {
+					// if the other array is a falsy value, return
+					if (!array) return false;
+
+					// compare lengths - can save a lot of time 
+					if (this.length != array.length) return false;
+
+					for (var i = 0, l = this.length; i < l; i++) {
+						// Check if we have nested arrays
+						if (this[i] instanceof Array && array[i] instanceof Array) {
+							// recurse into the nested arrays
+							if (!this[i].equals(array[i])) return false;
+						} else if (this[i] != array[i]) {
+							// Warning - two different object instances will never be equal: {x:20} != {x:20}
+							return false;
+						}
+					}
+					return true;
+				};
+				// Hide method from for-in loops
+				Object.defineProperty(Array.prototype, "equals", { enumerable: false });
+
 				this.areas = {};
 				this.plots = {};
 				this.links = {};
@@ -15349,10 +15374,18 @@
 						}
 					}
 
+					var found;
 					for (var i = 0; i < this.blockades.length; i++) {
-						if ($.inArray(this.blockades[i], diff['blockades']) == -1) {
-							this.hazard.OpenLink(this.blockades[i]);
+						found = false;
+						for (var j = 0; j < diff['blockades'].length; j++) {
+							if (this.blockades[i].split("-").sort().equals(diff['blockades'][j].locations.sort())) {
+								found = true;
+								continue;
+							}
+						}
+						if (!found) {
 							this.blockades.splice(i, 1);
+							this.hazard.OpenLink(this.blockades[i]);
 						}
 					}
 				}
